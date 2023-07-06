0bd50ee62eb0c0d96f1d39d33ceabec0888970ec
AdelWang <83192059+AdelWang@users.noreply.github.com>
2023-07-06 15:57:20 +0800

    Update train_and_inference.sh

--- train_and_inference.sh
+++ train_and_inference.sh
@@ -1,19 +1,17 @@
-# MODEL_NAME=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/wangkeheng/models/openqa/PLM/560m/
-source activate /mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/wangkeheng/project_conda
-source /opt/rh/devtoolset-8/enable
-
-MODEL_NAME='/mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/wangkeheng/models/openqa/PLM/llama-7b-hf'
+YOUR_DATA_DIR=''
+YOUR_OUTPUT_DIR=''
+MODEL_NAME="bigscience/bloom-7b"
 deepspeed --master_port=29500 main.py \
     --model_name_or_path $MODEL_NAME \
-    --data_dir /mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/wangkeheng/data/math/prm800k_sft/sub \
-    --output_dir /mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/wangkeheng/models_llm/math_debug/llama-7b \
-    --max_length 1024 \
+    --data_dir $YOUR_DATA_DIR \
+    --output_dir $YOUR_OUTPUT_DIR \
+    --max_length 2048 \
     --batch_size 64 \
-    --deepspeed_config /mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/wangkeheng/llm_project/llm_sft/data_utils/deepspeed_config.json \
-    --gradient_accumulation_steps 32 \
-    --eval_batch_size 2 \
+    --deepspeed_config ./data_utils/deepspeed_config.json \
+    --gradient_accumulation_steps 16 \
+    --eval_batch_size 4 \
     --num_train_epochs 1 \
-    --save_steps 100000 \
+    --save_steps 1000 \
     --lora \
     --learning_rate 2e-4 \
     --do_train \
